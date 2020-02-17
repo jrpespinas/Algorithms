7a009b61fb9625ad693f32a3918abb20f0aa836f
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-02-17 23:22:15 +0800

    feat: added a new class Node

--- linkedlist.py
+++ linkedlist.py
@@ -1,13 +1,21 @@
 """
     Data Structures and algorithms: Linked-list implementation using Python
 """
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__version__: '1.0.0'
+__author__: 'Jan Rodolf Espinas'
+
 class Node:
-    def __init__(self,head,value):
-        self.head = head 
-        self.value = value
+    def __init__(self, data, next = None):
+        self.data = data 
+        self.next = next
 
 def main():
-    print("Hello, world")
-    
+    A = Node("Head")    
+    print(A.data)
+
 if __name__ == "__main__":
     main()
