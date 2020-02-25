e0528072ae9462cecf0f299e619629846790ec7b
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-02-26 01:02:36 +0800

    feat: added a method InsertionSort()

--- insertion_sort.py
+++ insertion_sort.py
@@ -8,8 +8,24 @@ from __future__ import print_function
 __version__: '1.0.0'
 __author__: 'Jan Rodolf Espinas'
 
+def InsertionSort(A):
+    print(A)
+    # [j = 5, key(1) = 2, 4, 6, 1, 3]
+
+    for i in range(1, len(A)):
+        key = A[i]
+        j = i - 1
+        while key < A[j] and j >= 0:
+            A[j+1] = A[j]
+            j -= 1
+        A[j + 1] = key
+    return A
+
 def main():
-    print("Hello, world!")
+    print("Insertion Sort")
+    A = [5,2,4,6,1,3]
+    A = InsertionSort(A)
+    print(A)
 
 if __name__ == '__main__':
     main()
