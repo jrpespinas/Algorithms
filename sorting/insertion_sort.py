e3e3b8edb9bd7c31d14e5a84f4f88e5c8422d995
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-02-26 01:14:17 +0800

    chore: Fixed file structure

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -26,19 +26,12 @@ def insertion_sort(A):
     print(A)
     # [j = 5, key(1) = 2, 4, 6, 1, 3]
 
-    for i in range(1, len(A)):
-        key = A[i]
-        j = i - 1
-        while key < A[j] and j >= 0:
-            A[j+1] = A[j]
-            j -= 1
-        A[j + 1] = key
     return A
 
 def main():
     print("Insertion Sort")
     A = [5,2,4,6,1,3]
-    A = InsertionSort(A)
+    A = insertion_sort(A)
     print(A)
 
 if __name__ == '__main__':
