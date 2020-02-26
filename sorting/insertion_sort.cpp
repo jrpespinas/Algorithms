06972d20d80eac8309480efd7aad2881183e08b7
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-02-26 16:59:30 +0800

    feat: Added a C++ implementation of insertion sort

---
+++ sorting/insertion_sort.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void insertion_sort(int A[], int len){
+    for(int i = 1; i < len; i++){
+        int key = A[i];
+        int j = i-1;
+        while(key < A[j] && j >= 0){
+            A[j + 1] = A[j];
+            j -= 1;
+        }
+        A[j+1] = key;
+    }
+    for(int i = 0; i < len; i++){
+        cout << A[i] << " ";
+    }
+}
+int main(){
+    int len = 10;
+    int A[] = {8,7,5,3,2,1,6,3,8,4};
+    insertion_sort(A,len);
+
+    return 0;
+}
