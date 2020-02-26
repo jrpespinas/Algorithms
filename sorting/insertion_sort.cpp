b7fa5f0b305e0ff7e78b32fcea4ee08410eb90d4
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-02-26 17:28:40 +0800

    feat: Added a function to print an array

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -1,5 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+void printArray(int A[], int len){
+    for(int i = 0; i < len; i++){
+        cout << A[i] << " ";
+    }
+}
 
 void insertion_sort(int A[], int len){
     for(int i = 1; i < len; i++){
@@ -11,14 +17,12 @@ void insertion_sort(int A[], int len){
         }
         A[j+1] = key;
     }
-    for(int i = 0; i < len; i++){
-        cout << A[i] << " ";
-    }
+    printArray(A,len);
 }
+
 int main(){
     int len = 10;
     int A[] = {8,7,5,3,2,1,6,3,8,4};
     insertion_sort(A,len);
-
     return 0;
 }
