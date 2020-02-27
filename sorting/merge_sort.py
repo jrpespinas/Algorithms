55878b4f460086704e7a2e828ab7dd6c9e23d72c
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-02-27 23:59:11 +0800

    feat: Added a new script merge_sort.py

---
+++ sorting/merge_sort.py
@@ -0,0 +1,15 @@
+"""
+    Introduction to Algorithms: Merge Sort
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__version__: '1.0.0'
+__author__: 'Jan Rodolf Espinas'
+
+def main():
+    print("Hello, world")
+
+if __name__ == "__main__":
+    main()
