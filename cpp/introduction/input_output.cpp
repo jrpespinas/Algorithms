3c19d868cb770cf99b0e0ecb33e90c1e54311f06
jrpespinas <jrpespinas@gmail.com>
2021-02-02 20:31:32 +0800

    feat: add input function

---
+++ cpp/introduction/input_output.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void InputSample1();
+
+int main(){
+    InputSample1();
+    return 0;
+}
+
+void InputSample1(){
+    // ask the user for input using `cin`
+    int a, b;
+    string x;
+
+    cin >> a >> b >> x;
+}
