2ac333b17cae6ab3df0fcbba40b056c420491717
jrpespinas <jrpespinas@gmail.com>
2021-03-28 15:36:49 +0800

    feat: add capitalize first letter function

--- c/capitalize.c
+++ c/capitalize.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 
-const char* capslock(char str[], int length);
+const char* capslock(char str[]);
+const char* capitalize(char str[]);
 int string_length(char str[]);
 
 int main(){
     char str[1000];
     printf("please enter a string: ");
-    scanf("%s", str); 
+    scanf("%[^\n]s", str); 
     int length = string_length(str);
-    printf("%s", capslock(str, length));
+    printf("%s\n", capslock(str));
+    printf("%s\n", capitalize(str));
 }
 
-const char* capslock(char str[], int length){
+const char* capslock(char str[]){
     // capitalize all letters of the string
-    for (int i = 0; i < length; i++){
+    for (int i = 0; str[i] != '\0'; i++){
         if (str[i] >= 'a' && str[i] <= 'z')
             str[i] -= ('a' - 'A');
+    }
+    return str;
+}
+
+const char* capitalize(char str[]){
+    for (int i = 0; str[i] != '\0'; i++){
+        if (i == 0){
+            if (str[i] >= 'a' && str[i] <= 'z')
+                str[i] -= ('a' - 'A');
+        }
+        else if (str[i] == ' '){
+            i++;
+            if (str[i] >= 'a' && str[i] <= 'z')
+                str[i] -= ('a' - 'A');
+        }
+        else {
+            if (str[i] >= 'A' && str[i] <= 'Z')
+                str[i] += ('a' - 'A');
+        }
     }
     return str;
 }
