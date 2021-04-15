2b60806520cab8085423f32c5aa08920a52600d2
jrpespinas <jrpespinas@gmail.com>
2021-04-15 22:44:08 +0800

    feat: add decrypt function

--- c/cipher.c
+++ c/cipher.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
-const char* encrypt(char str[]);
+char* encrypt(char str[]);
+char* decrypt(char str[]);
 
 int main(){
     char str[1000];
     printf("Enter a string: ");
     scanf("%[^\n]s", str);
-    printf("%s\n", encrypt(str));
+    printf("%s\n", decrypt(encrypt(str)));
     return 0;
 }
 
-const char* encrypt(char str[]){
+char* encrypt(char str[]){
     for(int i = 0; str[i] != '\0'; i++){
         str[i] += 1; 
     }
-    return str; 
+    return str;
+}
+
+char* decrypt(char str[]){
+    for(int i = 0; str[i] != '\0'; i++){
+        str[i] -= 1;
+    }
+    return str;
 }
