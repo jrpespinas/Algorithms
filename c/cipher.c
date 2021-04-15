2413a5d567df62cffe4a0e8bf057eb7563f0105d
jrpespinas <jrpespinas@gmail.com>
2021-04-15 22:28:12 +0800

    feat: add encrypt function

--- c/cipher.c
+++ c/cipher.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+const char* encrypt(char str[]);
+
 int main(){
+    char str[1000];
+    printf("Enter a string: ");
+    scanf("%[^\n]s", str);
+    printf("%s\n", encrypt(str));
     return 0;
+}
+
+const char* encrypt(char str[]){
+    for(int i = 0; str[i] != '\0'; i++){
+        str[i] += 1; 
+    }
+    return str; 
 }
