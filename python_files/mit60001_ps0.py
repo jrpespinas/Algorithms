7b69d26298146a63808300737bbf0ca54867459c
Bogs Espinas <jrpespinas@gmail.com>
2021-05-12 16:46:47 +0800

    feat: add problem set 0

---
+++ python_files/mit60001_ps0.py
@@ -0,0 +1,30 @@
+"""
+MIT 6.0001 - Introduction to Computer Science and Programming in Python
+Problem Set 0
+-------------
+
+1. Ask the user to enter a number `x`
+2. Ask the user to enter a number `y`
+3. Prints out number `x`, raised to the power of `y`
+4. Prints out the log (base 2) of `x`
+"""
+import numpy as np
+
+
+def raised_to_y(x: int, y: int):
+    return x ** y
+
+
+def log_base_2(x: int):
+    return np.log2(x)
+
+
+def main():
+    x = int(input("X: "))
+    y = int(input("y: "))
+    print(raised_to_y(x, y))
+    print(log_base_2(x))
+
+
+if __name__ == "__main__":
+    main()
