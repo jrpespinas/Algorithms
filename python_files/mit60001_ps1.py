91129f8f27f3468dd3b287c48de9a2c2f6fe9a55
Bogs Espinas <jrpespinas@gmail.com>
2021-05-13 21:35:45 +0800

    chore: add template for problem set 1

---
+++ python_files/mit60001_ps1.py
@@ -0,0 +1,15 @@
+"""
+MIT 6.0001 - Introduction to Computer Science and Programming in Python
+Problem Set 1
+-------------
+
+1. ...
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
