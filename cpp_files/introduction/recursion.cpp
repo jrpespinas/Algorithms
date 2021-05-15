3e089751a2bed8ac8501ddc2262e6548e41e8de3
Bogs Espinas <jrpespinas@gmail.com>
2021-05-15 23:26:22 +0800

    refactor: implement better exit message

--- cpp_files/introduction/recursion.cpp
+++ cpp_files/introduction/recursion.cpp
@@ -12,16 +12,25 @@ int main(){
     int start, end;
     cin >> word >> start >> end;
 
-    reverseSubstringRecursion(word, start - 1, end - 1);
-    cout << word;
+    // reverseSubstringRecursion(word, start - 1, end - 1);
+    // cout << word;
+
+    cout << reverseSubstringLoop(word, start, end);
+
     return 0;
 }
 
 string reverseSubstringLoop(string word, int start, int end){
     int length = word.length();
-    
+    start -= 1;
+    end -= 1;
+
+    if (checkConstraint(length, start, end) == 0) {
+        return "Exiting";
+    }
+
     for (int i = 0; i < length; i++) {
-        if (i >= start - 1 && i <= end - 1) {
+        if (i >= start && i <= end) {
             swap(word[i], word[end]);
             end = end - 1;
         }
@@ -54,15 +63,15 @@ void reverseSubstringRecursion(string &word, int start, int end, int i) {
 
 int checkConstraint(int length, int start, int end) {
     if (length > 100 || length < 1){ 
-        cout << "String exceeds specified length, ergo no changes!";
+        cout << "String exceeds specified length, ergo no changes!\n";
         return 0;
     }
     if (start >= end || start < 0) {
-        cout << "Starting value falls outside of range!";
+        cout << "Starting value falls outside of range!\n";
         return 0;
     }
     if (end <= start || end >= length) {
-        cout << "Ending value falls outside of range!";
+        cout << "Ending value falls outside of range!\n";
         return 0;
     }
     return 1;       
