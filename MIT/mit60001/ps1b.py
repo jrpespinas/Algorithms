85044cc10e46d2089f4a139314b28b79cada94ea
Bogs Espinas <jrpespinas@gmail.com>
2021-05-21 14:31:37 +0800

    feat: implement get salary raise method

--- MIT/mit60001/ps1b.py
+++ MIT/mit60001/ps1b.py
@@ -3,7 +3,10 @@ ANNUAL_SAVINGS_RATE = 0.04
 
 
 def number_of_months_to_save(
-    annual_salary: float, portion_saved: float, total_cost: float
+    annual_salary: float,
+    portion_saved: float,
+    total_cost: float,
+    semi_annual_raise: float,
 ) -> int:
 
     current_savings = 0
@@ -20,7 +23,14 @@ def number_of_months_to_save(
 
         months += 1
 
+        if months % 6 == 0:
+            monthly_salary = get_salary_raise(monthly_salary, semi_annual_raise)
+
     return months
+
+
+def get_salary_raise(monthly_salary: float, semi_annual_raise: float) -> float:
+    return monthly_salary + (monthly_salary * semi_annual_raise)
 
 
 def get_savings_portion(monthly_salary: float, portion_saved: float) -> float:
@@ -45,8 +55,12 @@ def main():
         input("Enter the percent of your salary to save, as a decimal: ")
     )
     total_cost = float(input("Enter the cost of your dream house: "))
+    semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
+    months = number_of_months_to_save(
+        annual_salary, portion_saved, total_cost, semi_annual_raise
+    )
 
-    print(number_of_months_to_save(annual_salary, portion_saved, total_cost))
+    print("Number of months", months)
 
 
 if __name__ == "__main__":
