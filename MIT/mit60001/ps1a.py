763484e79c2b307876d1ae9a3b13942eb0ff93e5
Bogs Espinas <jrpespinas@gmail.com>
2021-05-21 14:13:10 +0800

    feat: implement method which calculates number of months to save

--- MIT/mit60001/ps1a.py
+++ MIT/mit60001/ps1a.py
@@ -47,23 +47,35 @@ Test Case 2
 """
 
 PORTION_DOWN_PAYMENT = 0.25
-PORTION_SAVED = 0.1
+ANNUAL_SAVINGS_RATE = 0.04
 
 
 def number_of_months_to_save(
     annual_salary: float, portion_saved: float, total_cost: float
 ) -> int:
-    portion_down_payment = get_down_payment(total_cost)
-    portion_saved = get_portion_saved(annual_salary)
 
-    return number_of_months
+    current_savings = 0
+    portion_down_payment = get_portion_down_payment(total_cost)
+    monthly_salary = get_monthly_salary(annual_salary)
+
+    months = 0
+    while current_savings <= portion_down_payment:
+        current_savings = (
+            current_savings
+            + get_savings_portion(monthly_salary, portion_saved)
+            + get_investments_return(current_savings)
+        )
+
+        months += 1
+
+    return months
 
 
-def get_portion_saved(annual_salary: float) -> float:
-    return get_monthly_salary(annual_salary) * PORTION_SAVED
+def get_savings_portion(monthly_salary: float, portion_saved: float) -> float:
+    return monthly_salary * portion_saved
 
 
-def get_down_payment(total_cost: float) -> float:
+def get_portion_down_payment(total_cost: float) -> float:
     return total_cost * PORTION_DOWN_PAYMENT
 
 
@@ -71,17 +83,18 @@ def get_monthly_salary(annual_salary: float) -> float:
     return annual_salary / 12
 
 
+def get_investments_return(current_savings: float) -> float:
+    return current_savings * ANNUAL_SAVINGS_RATE / 12
+
+
 def main():
     annual_salary = float(input("Enter your annual salary: "))
     portion_saved = float(
-        input("Enter the percent oif your salary to save, as a decimal: ")
+        input("Enter the percent of your salary to save, as a decimal: ")
     )
-    total_cost = float(input("Enter the cost of your dream home: "))
-    number_of_months = number_of_months_to_save(
-        annual_salary, portion_saved, total_cost
-    )
+    total_cost = float(input("Enter the cost of your dream house: "))
 
-    print("Number of months:", number_of_months)
+    print(number_of_months_to_save(annual_salary, portion_saved, total_cost))
 
 
 if __name__ == "__main__":
