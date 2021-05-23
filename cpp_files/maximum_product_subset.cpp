52c098a0d0e9edf959ee7b655f641b3b416d2618
Bogs Espinas <jrpespinas@gmail.com>
2021-05-23 22:21:49 +0800

    feat: write method which returns maximum product

--- cpp_files/maximum_product_subset.cpp
+++ cpp_files/maximum_product_subset.cpp
@@ -6,23 +6,40 @@ using namespace std;
 #define MIN -100000
 
 void input_set(int array[], int n);
+int get_maximum_product(int array[], int n);
 
 int main() {
     int n;
-    int array[MAX];
     cin >> n;
 
+    if (n > MAX) {
+        cout << "ERROR: Array size is too big!";
+        return 0;
+    }
+
+    int array[n];
+
     input_set(array, n);
+    cout << get_maximum_product(array, n);
 
     return 0;
 }
 
 void input_set(int array[], int n) {
+    int num;
     for (int i = 0; i < n; i++) {
-        cin >> array[i];
-        if (array[i] >= MAX or array[i] <= MIN) {
+        cin >> num;
+
+        if (num >= MAX or num <= MIN) {
             cout << "ERROR: input value exceeds range!";
             break;
+        } else {
+            array[i] = num;
         }
     }
 }
+
+int get_maximum_product(int array[], int n) {
+    sort(array, array+n);
+    return array[n-1] * array[n-2];
+} 
