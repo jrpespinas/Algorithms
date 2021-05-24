026dc89c85294e3bac0554191c61cc329194c789
Bogs Espinas <jrpespinas@gmail.com>
2021-05-24 22:23:58 +0800

    test: write tests for problem set 1C

--- MIT/mit60001_tests/ps1.py
+++ MIT/mit60001_tests/ps1.py
@@ -1,5 +1,5 @@
 import pytest
-from mit60001 import ps1a, ps1b
+from mit60001 import ps1a, ps1b, ps1c
 
 
 @pytest.mark.parametrize(
@@ -26,3 +26,17 @@ def test_problem_set_1b(
         annual_salary, portion_saved, total_cost, semi_annual_raise
     )
     assert output == expected_result
+
+
+@pytest.mark.parametrize(
+    "starting_salary, savings_rate, bisection_steps",
+    [
+        (150000, 0.4411, 12),
+        (300000, 0.2206, 13),
+        (10000, 0.0, 0.0),
+    ],
+)
+def test_problem_set_1c(starting_salary, savings_rate, bisection_steps):
+    output_savings, output_bisection_step = ps1c.bisection_search(starting_salary)
+    assert output_bisection_step == bisection_steps
+    assert abs(output_savings - savings_rate) <= 0.03
