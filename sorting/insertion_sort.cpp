1305186f2d1bba240c1ec9a22c1e2ef54e911a6d
Bogs Espinas <jrpespinas@gmail.com>
2021-07-18 19:31:09 +0800

    refactor: rewrite methods

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void insertion_sort(int arr[], int size) {
-	for(int i=1; i<size; i++) {
-		int j = i - 1;
-		int key = arr[i];
-		while (key < arr[j] && j >= 0) {
-			arr[j + 1] = arr[j]; 
-			j--;
-		}
-		arr[j + 1] = key;
-	}
+void insertion_sort(int *arr, int size) {	
+    for(int i=0; i<size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (key <= arr[j] && j >= 0) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
 }
 
 
