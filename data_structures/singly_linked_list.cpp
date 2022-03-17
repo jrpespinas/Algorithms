8ab7cec0ee1c524e42c1ce5745eb077d8bc2e079
Bogs Espinas <jrpespinas@gmail.com>
2022-03-17 11:44:49 +0800

    feat: implement Node class

--- data_structures/singly_linked_list.cpp
+++ data_structures/singly_linked_list.cpp
@@ -1,97 +1,22 @@
 #include <iostream>
 using namespace std;
 
-class Node{
+class Node {
     public:
         int data;
         Node *next;
-        Node(int data) {
+        Node (int data) {
             this->data = data;
             this->next = nullptr;
         }
 };
 
-class List {
-    Node *head;
-    public:
-        List() {
-            this->head = nullptr;
-        }
-        void prepend(int data) {
-            Node *node = new Node(data);
-            node->next = this->head;
-            head = node;
-            return;
-        }
-        void append(int data) {
-            if (this->head == nullptr) {
-                this->prepend(data);
-                return;
-            } else {
-                Node *ptr = this->head;
-                while(ptr->next != nullptr) {
-                    ptr = ptr->next;
-                }
-                Node *node = new Node(data);
-                ptr->next = node;
-                return;
-            }
-        }
-        void display() {
-            Node *ptr = this->head;
-            while (ptr->next != nullptr) {
-                cout << ptr->data << " ";
-                ptr = ptr->next;
-            }
-            cout << ptr->data << "\n";
-            return;
-        }
-        void popHead() {
-            this->head = this->head->next;           
-            return;
-        }
-        void deleteVal(int data) {
-            if (this->head->data == data) {
-                popHead();
-                return;
-            }
-            Node *ptr = this->head;
-            while(ptr->next != nullptr) {
-                if (ptr->next->data == data) {
-                    ptr->next = ptr->next->next;
-                }
-                ptr = ptr->next;
-            }
-            return;
-        }
-        void pop() {
-            Node *ptr = this->head;
-            while(ptr->next->next != nullptr) {
-                ptr = ptr->next;
-            }
-            ptr->next = nullptr;
-            return;
-        }
-        void reverse() {
-            if (this->head->next == nullptr)
-                return;
-            else {
-                Node *prev = nullptr;
-                Node *curr = this->head;
-                Node *next = nullptr;
 
-                while (curr != nullptr) {
-                    next = curr->next;
-                    curr->next = prev;
-                    prev = curr;
-                    curr = next;
-                }
-                this->head = prev;
-            }
-        }
-};
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     List nums;
     nums.append(1);
     nums.append(2);
@@ -103,4 +28,5 @@ int main() {
     nums.reverse();
     nums.popHead();
     nums.display();
+    return 0;
 }
