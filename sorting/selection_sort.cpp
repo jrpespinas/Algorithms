2d907cb7c8a9fe688dabfd9133438b096740fdb1
Bogs Espinas <jrpespinas@gmail.com>
2022-04-02 15:45:36 +0800

    feat: implement selection sort

--- sorting/selection_sort.cpp
+++ sorting/selection_sort.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
 
-void selection_sort(int *arr, int size) {	
-	for (int i=0; i<size; i++) {
+void selection_sort(int *arr, int size) {
+    for (int i=0; i<size; i++) {
 		int j=i;
 		int minimum = j;
 		while (j < size) {
-			if (arr[j] < arr[minimum])
-				minimum = j;
+			if (arr[j] < arr[minimum]) minimum = j;
 			j++;
 		}
 		swap(arr[i], arr[minimum]);
@@ -20,8 +19,10 @@ void display(int *arr, int size) {
 	}
 }
 
-
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
 	int arr[] = {3, 7, 12, 2, 13, 18, 24, 3, 8};	
 	int size = sizeof(arr) / sizeof(int);
 	selection_sort(arr, size);
