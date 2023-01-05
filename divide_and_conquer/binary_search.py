44353e015998d666290482d0272538428fa3200a
jrpespinas <jrpespinas@gmail.com>
2023-01-05 20:59:31 +0800

    feat: implement recursion

--- divide_and_conquer/binary_search.py
+++ divide_and_conquer/binary_search.py
@@ -1,6 +1,6 @@
-def search(array, item):
+def search_iterative(array, item):
     """a function which divides the input per iteration
-    until the guess is equal to the item being search"""
+    until the guess is equal to the item being searched"""
     low = 0
     high = len(array) - 1
 
@@ -8,11 +8,30 @@ def search(array, item):
         mid = (low + high) // 2
         guess = array[mid]
 
-        if guess < item:
-            low = mid + 1
-        elif guess > item:
-            high = mid - 1
-        else:
+        if guess == item:
             return mid
+        elif guess < item:
+            low = mid + 1
+        else:
+            high = mid - 1
 
     return None
+
+def search_recursive(array, item, high, low):
+    """a recursive function which divides the input per Step
+    until the guess is equal to the item being searched
+    """
+    if low <= high:
+        mid = (high + low) // 2
+        guess = array[mid]
+
+        if guess == item:
+            return mid
+        elif guess < item:
+            return search_recursive(array, item, high, mid + 1)
+        else:
+            return search_recursive(array, item, mid - 1, low)
+    else:
+        return None
+
+
